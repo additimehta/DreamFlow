4d8a245ca057aa595e41b8b93f4ce55d162e0eeb
additi123 <mehtaadditi@gmail.com>
2025-04-11 13:27:22 -0400

    basic idea ig

---
+++ backend/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import bcrypt
+import jwt
+import datetime
+
+
+
+# POST method for sign up
+
+
+# POST method for login 
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+    
